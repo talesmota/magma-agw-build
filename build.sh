c22171653733327d754f18cc3671ed87ab26a426
Tales Mota Machado <talesmmachado@gmail.com>
2022-02-04 13:57:19 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,24 +2,28 @@
 
 set -ex
 
-# Install prerequisites
-pip3 install ansible fabric3 jsonpickle requests PyYAML
-vagrant plugin install vagrant-vbguest
-
 mkdir -p ~/.ssh
 touch ~/.ssh/id_rsa
 touch ~/.ssh/id_rsa.pub
 
-echo "${ID_RSA}" | base64 -d > ~/.ssh/id_rsa
-echo "${ID_PUB}" | base64 -d > ~/.ssh/id_rsa.pub
+echo $ID_RSA | base64 -d > ~/.ssh/id_rsa
+echo $ID_PUB | base64 -d > ~/.ssh/id_rsa.pub
+
+cat ~/.ssh/id_rsa.pub
+
+# Install prerequisites
+#pip3 install ansible fabric3 jsonpickle requests PyYAML
+#vagrant plugin install vagrant-vbguest
+
+
 
 # Cloning magma repo:
-git clone -b release/1.9.0 git@bitbucket.org:radtonics/rt-core.git --depth 1
+#git clone -b release/1.9.0 git@bitbucket.org:radtonics/rt-core.git --depth 1
 
 # start building magma
 cd magma/lte/gateway
 sed -i '' 's/1.1.20210928/1.1.20210618/' Vagrantfile
-fab release package:vcs=git
+#fab release package:vcs=git
 
 # copy magma packages to github runner
-vagrant ssh -c "cp -r magma-packages /vagrant"
+#vagrant ssh -c "cp -r magma-packages /vagrant"
