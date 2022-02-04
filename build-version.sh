b22dd033182d6a33aba454ae36763ae95d7710d1
Tales Mota Machado <talesmmachado@gmail.com>
2022-02-04 14:13:08 -0300

    Update build-version.sh

--- build-version.sh
+++ build-version.sh
@@ -13,16 +13,16 @@ echo $ID_PUB | base64 -d > ~/.ssh/id_rsa.pub
 
 cat ~/.ssh/id_rsa
 
-
+# Cloning magma repo:
+git clone -b release/1.9.0 git@bitbucket.org:radtonics/rt-core.git --depth 1
 
 # Install prerequisites
-#pip3 install ansible fabric3 jsonpickle requests PyYAML
-#vagrant plugin install vagrant-vbguest vagrant-disksize vagrant-vbguest vagrant-mutate
+pip3 install ansible fabric3 jsonpickle requests PyYAML
+vagrant plugin install vagrant-vbguest vagrant-disksize vagrant-vbguest vagrant-mutate
 
 
 
-# Cloning magma repo:
-#git clone -b release/1.9.0 git@bitbucket.org:radtonics/rt-core.git --depth 1
+
 
 # Cloning magma repo:
 #git clone -b $1 https://github.com/magma/magma --depth 1
@@ -38,9 +38,9 @@ MAGMA_ROOT=${PWD}/magma
 
 # start building magma
 cd ${MAGMA_ROOT}/lte/gateway
-#sed -i '' 's/1.1.20210928/1.1.20210618/' Vagrantfile
+sed -i '' 's/1.1.20210928/1.1.20210618/' Vagrantfile
 
-#fab $2 package:vcs=git
+fab $2 package:vcs=git
 
 # copy magma packages to github runner
-#vagrant ssh -c "cp -r magma-packages /vagrant"
+vagrant ssh -c "cp -r magma-packages /vagrant"
